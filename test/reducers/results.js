65b6f3600b3bf9b00cd4b05f18b05dcb2a19c113
renevanderark <r.van.der.ark@gmail.com>
2016-06-24 17:13:29 +0200

    adds test for results reducer.

---
+++ test/reducers/results.js
@@ -0,0 +1,43 @@
+import expect from "expect";
+
+import resultsReducer from "../../src/reducers/results";
+
+describe("resultsReducer", () => { //eslint-disable-line no-undef
+
+	it("should SET_RESULTS", () => { //eslint-disable-line no-undef
+		expect(resultsReducer({
+			init: "bar",
+			pending: true
+		}, {
+			type: "SET_RESULTS",
+			data: {
+				response: {
+					docs: ["123"],
+					numFound: 123
+			},
+			"facet_counts": {
+				"facet_fields": ["123"]
+			}
+			}
+		})).toEqual({
+			init: "bar",
+			docs: ["123"],
+			numFound: 123,
+			facets: ["123"],
+			pending: false
+		});
+	});
+
+	it("should SET_RESULTS_PENDING", () => { //eslint-disable-line no-undef
+		expect(resultsReducer({
+			init: "bar",
+			pending: false
+		}, {
+			type: "SET_RESULTS_PENDING"
+		})).toEqual({
+			init: "bar",
+			pending: true
+		});
+	});
+
+});
